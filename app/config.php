32aeaa401f993f32f07d7ecfbacbcbaa7344b89f
sridattaga <sivag.soc95@gmail.com>
2026-04-25 22:23:41 +0530

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,8 +1,6 @@
 <?php
-define('DB_HOST', getenv('DB_HOST'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASS', getenv('DB_PASS'));
-define('DB_NAME', getenv('DB_NAME'));
-
-define('S3_BUCKET', getenv('S3_BUCKET'));
+define('DB_HOST', getenv('DB_HOST') ?: 'mysql-service');
+define('DB_USER', getenv('DB_USER') ?: 'user');
+define('DB_PASS', getenv('DB_PASS') ?: 'password123');
+define('DB_NAME', getenv('DB_NAME') ?: 'ecommerce');
 ?>
